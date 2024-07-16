9feff3314f681f68d7117fd24421652673c93c3f
Andressa Rodrigues <solutionmind.digital@gmail.com>
2024-07-16 08:31:49 -0300

    subindo alterações

---
+++ cypress/e2e/3-loja-ebac/login.cy.js
@@ -0,0 +1,14 @@
+/// <reference types="cypress"/>
+
+describe('Funcionalidade: Login', () => {
+
+    it('Deve fazer login com sucesso', () => {
+        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+        cy.get('#username').type('andressateste@mailinator.com')
+        cy.get('#password').type('Senha123456@')
+        cy.get('.woocommerce-form > .button').click()
+
+        cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá, andressateste (não é andressateste? Sair)')
+    })
+
+} )
