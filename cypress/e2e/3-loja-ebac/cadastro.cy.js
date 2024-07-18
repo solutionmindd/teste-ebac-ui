0060957a4b009cefd9ae7143d29d818d5a40213b
Andressa Rodrigues <solutionmind.digital@gmail.com>
2024-07-18 14:01:07 -0300

    alterações aula 4

--- cypress/e2e/3-loja-ebac/cadastro.cy.js
+++ cypress/e2e/3-loja-ebac/cadastro.cy.js
@@ -13,9 +13,17 @@ describe('Funcionalidade: Cadastro', () => {
     });
 
     it('Deve completar o cadastro com sucesso.', () => {
-       cy.get('#reg_email').type(faker.internet.email())
+       var email = faker.internet.email()
+       var nome =  faker.person.firstName()
+       var sobrenome = faker.person.lastName()
+       cy.get('#reg_email').type(email)
        cy.get('#reg_password').type('Senha123456@')
        cy.get(':nth-child(4) > .button').click()
        cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('exist')
+       cy.get('.woocommerce-MyAccount-navigation-link--edit-account > a').click()
+       cy.get('#account_first_name').type(nome)
+       cy.get('#account_last_name').type(sobrenome)
+       cy.get('.woocommerce-Button').click()
+       cy.get('.woocommerce-message').should('exist')
     });
 });
